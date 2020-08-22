5ca5d27fccacba2d8ea30b91744b6cd1cd85a276
Sydnie Chau <40672145+sydnieec@users.noreply.github.com>
2020-08-22 15:46:35 -0400

    restautant login form functional

--- website/src/pages/RestaurantLogin.jsx
+++ website/src/pages/RestaurantLogin.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import FormControl from "react-bootstrap/FormControl";
@@ -7,41 +7,65 @@ import { Link } from "react-router-dom";
 import Nav from "../Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-function RestaurantLogin() {
-  const loginStyle = {
-    paddingRight: "30%",
-    paddingLeft: "30%",
+class RestaurantLogin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loginemail: "", loginpassword: "" };
+  }
+  handleLoginEmailChange = (event) => {
+    this.setState({ loginemail: event.target.value });
   };
-  return (
-    <React.Fragment>
-      <Nav />
-      {/* <Navbar bg="primary" variant="dark">
-        <Link to="/">
-          <Navbar.Brand href="#home">Logo</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle />
-      </Navbar>{" "} */}
-      <div style={loginStyle}>
-        <h1> Login as a Food Provider </h1>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
+  handleLoginPasswordChange = (event) => {
+    this.setState({ loginpassword: event.target.value });
+  };
+  handleSubmit = (event) => {
+    alert(this.state.loginemail + this.state.loginpassword);
+    event.preventDefault();
+  };
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Link to="/restaurantform">
+  render() {
+    return (
+      <React.Fragment>
+        <Nav />
+        <div style={loginStyle}>
+          <h1> Login as a Food Provider </h1>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                value={this.state.loginemail}
+                onChange={this.handleLoginEmailChange}
+                type="email"
+                placeholder="Enter email"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                value={this.state.loginpassword}
+                onChange={this.handleLoginPasswordChange}
+                // type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
             <Button variant="primary" type="submit">
-              Login
-            </Button>
-          </Link>
-        </Form>
-      </div>
-    </React.Fragment>
-  );
+              Submit
+            </Button>{" "}
+            <Link to="/restaurantform">
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Link>
+          </Form>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default RestaurantLogin;
+
+const loginStyle = {
+  paddingRight: "30%",
+  paddingLeft: "30%",
+};
