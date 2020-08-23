6ca779b6ca069e19f0f4d53fe46c1c31ff461314
Sydnie Chau <40672145+sydnieec@users.noreply.github.com>
2020-08-22 22:42:57 -0400

    cards for all forms made

--- website/src/pages/RestaurantSignup.jsx
+++ website/src/pages/RestaurantSignup.jsx
@@ -58,7 +58,17 @@ class RestaurantSignup extends Component {
         <Navbar.Toggle />
       </Navbar>{" "} */}
         <div style={loginStyle}>
-          <h1> Sign up as a Food Provider </h1>
+          <h1
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingBottom: "5%",
+            }}
+          >
+            {" "}
+            Sign up as a Food Provider{" "}
+          </h1>
           <Form style={formStyle} onSubmit={this.handleSubmit}>
             <Form.Row>
               <Form.Group as={Col} controlId="formGridEmail">
@@ -135,5 +145,11 @@ const loginStyle = {
   paddingLeft: "30%",
 };
 const formStyle = {
-  paddingTop: "5%",
+  paddingTop: "15%",
+  backgroundColor: "#ff8364",
+  paddingRight: "10%",
+  paddingLeft: "10%",
+  paddingBottom: "20%",
+  borderRadius: "21px",
+  color: "white",
 };
